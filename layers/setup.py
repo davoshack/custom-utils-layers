16c8b40d6a59b6f6328b20d6f9204d89d390fcf2
Juan D Hernandez G <juandavos941@gmail.com>
2024-10-29 20:19:29 -0500

    Llama index custom utils

---
+++ layers/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="custom_utils",
+    version="0.0.1",
+    description="DeepLearning utils",
+    url="",
+    author="Davoscode",
+    author_email="dev@davoscode.com",
+    license="MIT",
+    packages=["custom_utils",],
+    zip_safe=False,
+)
